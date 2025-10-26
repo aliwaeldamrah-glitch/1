0348b96134f1395f74b60270efccf41cfc841ca7
aliwaeldamrah-glitch <aliwaeldamrah@gmail.com>
2025-10-26 18:05:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Console.WriteLine("welcome i am ali damra ");
+//Console.WriteLine("tell me your name ! ");
+//Console.WriteLine("your First name");
+//string Fname = Console.ReadLine();
+//Console.WriteLine("your last name");
+//string lname = Console.ReadLine();
+//Console.WriteLine($"welcome {Fname} {lname} ");
+
+
+Console.WriteLine("ok now Write a number between 1 and 7: ");
+int day = int.Parse(Console.ReadLine());
+
+if (day == 1)
+{ Console.WriteLine("Sunday"); }
+else if (day == 2)
+{ Console.WriteLine("Monday"); }
+else if (day == 3)
+{ Console.WriteLine("Tuesday"); }
+else if (day == 4)
+{ Console.WriteLine("Wednesday"); }
+else if (day == 5)
+{ Console.WriteLine("Thursday"); }
+else if (day == 6)
+{ Console.WriteLine("Friday"); }
+else if (day == 7)
+{ Console.WriteLine("Saturday"); }
+
+else
+{ Console.WriteLine("Invalid input"); }
+
+
